9bff54c7b4b87231d3fb1ce566989d6584e9165b
Dohyun Kim <dohyunkim-git@users.noreply.github.com>
2019-03-27 08:13:15 +0900

    190326
    파이썬 시작

---
+++ hello_world.py
@@ -0,0 +1,4 @@
+print("Hello Python world!")
+message="Hello Python World!"
+print(message)
+
---
+++ name.py
@@ -0,0 +1,28 @@
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "김"
+last_name = "도현"
+full_name = first_name+last_name;
+print(full_name)
+
+hello = "안녕, "+full_name+"!"
+print(hello);
+
+print("\tPython")
+print("\n먹고싶은것들 :\n치킨\n피자\n떡볶이\n라면\n")
+print("먹고싶은것들 :\n\t치킨\n\t피자\n\t떡볶이\n\t라면")
+
+learning_lang = 'python '
+print(learning_lang)
+print(learning_lang.rstrip())
+print(learning_lang)
+learning_lang = learning_lang.rstrip()
+print(learning_lang)
+
+learning_lang = ' python'
+learning_lang.lstrip()
+
+print('\n\n"sadf"')
